289ef2a97973d66584f135c94ed9e785f74a2854
Vishal Barot <97306828+Shiv0063@users.noreply.github.com>
2024-04-30 18:04:31 +0530

    Update settings.py

--- FarmerHouse/settings.py
+++ FarmerHouse/settings.py
@@ -26,7 +26,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'main/media')
 SECRET_KEY = 'django-insecure-6^fu+dhr22zj!=%-pvkn^lgervj*xjo45-m@m6e8($z8#$+nlk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
