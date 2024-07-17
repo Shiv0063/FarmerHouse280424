cb87410fe182d041f8eb25d9027dd49e9de7b5bc
Vishal Barot <97306828+Shiv0063@users.noreply.github.com>
2024-07-17 12:04:30 +0530

    Update settings.py

--- FarmerHouse/settings.py
+++ FarmerHouse/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
